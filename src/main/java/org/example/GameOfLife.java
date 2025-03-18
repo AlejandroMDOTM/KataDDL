ec6cd7c339bc92b368ac1c5c95314495bf2b4845
AlejandroMDOTM <alejandro.romera@mdotm.ai>
2025-03-18 12:09:52 +0100

    First Test Passed

--- src/main/java/org/example/GameOfLife.java
+++ src/main/java/org/example/GameOfLife.java
@@ -18,6 +18,8 @@ public class GameOfLife {
     public static boolean[][] gameOfLife(boolean[][] board) {
         for (int row = 0; row < board.length; row++) {
             for (int col = 0; col < board[row].length; col++) {
+                if (neighboursCheckOneOrZero(board, row, col))
+                    board[row][col]=false;
                 if (board[row][col])
                     System.out.print("*");
                 else
@@ -28,5 +30,21 @@ public class GameOfLife {
         return board;
     }
 
+    public static boolean neighboursCheckOneOrZero(boolean[][] board, int row, int col) {
+        int trueCount = 0;
+        for (int i = -1; i <= 1; i++) {
+            for (int j = -1; j <= 1; j++) {
+                if (i == 0 && j == 0) continue;
+                int neighbourRow = row + i;
+                int neighbourCol = col + j;
+                if (neighbourRow >= 0 && neighbourRow<board.length && neighbourCol >= 0 && neighbourCol < board[neighbourRow].length) {
+                    if (board[neighbourRow][neighbourCol]) {
+                        trueCount++;
+                    }
+                }
+            }
+        }
+     return trueCount<2 ? true : false;
+    }
 
 }
